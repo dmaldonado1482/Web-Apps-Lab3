74c159b57597447bf32b4e7b316433c1fbd59018
dmaldonado1482 <dmaldonado1482@gmail.com>
2018-10-03 18:04:54 -0400

    Added functionality for processing the form.

--- Lab3/wwwroot/processing.js
+++ Lab3/wwwroot/processing.js
@@ -9,14 +9,44 @@
 let submitBtn = document.querySelector('#bev-submit');
 let p = document.querySelector('#tester');
 let cust = document.querySelector('#cust');
-let items = document.querySelector('items');
+let items = document.querySelector('#items');
 let total = document.querySelector('#total');
-submitBtn.onclick = function () {
-    let beverage = document.querySelector('#beverage').value;
-    let quantity = document.querySelector('#quantity').value;
 
-    
-    p.innerHTML = 'test';
-    cust.innerHTML = "test";
+let prices1 = [
+    { bev: "Coffee", price: 4.99 },
+    { bev: "Tea", price: 3.99 },
+    { bev: "Beer", price: 6.99 },
+    { bev: "Wine", price: 12.99 }
+];
+
+let prices = {
+    "Coffee": 4.99,
+    "Tea": 3.99,
+    "Beer": 6.99,
+    "Wine": 12.99
+};
+
+submitBtn.onclick = function () {
+    let beverage = document.querySelector('#beverage');
+    let i = beverage.selectedIndex;
+    let bevChoice = beverage.options[i].text;   // beverage option
+    let quantity = Number(document.querySelector('#quantity').value);
+    let customer = document.querySelector('#custName').value;
+
+    let birthdate = new Date(document.querySelector('#birthdate').value);
+    let birthdateMoment = moment(birthdate);
+    let birthdateFormatted = birthdateMoment.format("l");
+    let date = moment().subtract(21, 'years').calendar();
+
+    if (date < birthdateMoment) {
+        p.innerHTML = "Test";
+    }
+    else {
+        p.innerHTML = "TESTTESTTEST";
+    }
+
+    cust.innerHTML = customer;
+    items.innerHTML = quantity + ' ' + bevChoice + ' @ ' + prices[bevChoice];
+    total.innerHTML = 'Total: $' + (prices[bevChoice] * quantity);
 
 };
