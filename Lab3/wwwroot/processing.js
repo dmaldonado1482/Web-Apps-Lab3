73729050582564b4c9fa7e64777ba84ce946d0f9
David Maldonado <dmaldonado1482@gmail.com>
2018-10-03 00:12:22 -0400

    Testing JS

--- Lab3/wwwroot/processing.js
+++ Lab3/wwwroot/processing.js
@@ -1,1 +1,22 @@
-var submit = document.getElementById("submit");
+// let form = document.getElementById('beverage-form');
+// document.getElementById("beverage-submit").addEventListener("click", function(){
+//     form.submit();
+// });
+
+// let x = document.forms[0].elements[0];
+// document.write(x);
+
+let submitBtn = document.querySelector('#bev-submit');
+let p = document.querySelector('#tester');
+let cust = document.querySelector('#cust');
+let items = document.querySelector('items');
+let total = document.querySelector('#total');
+submitBtn.onclick = function () {
+    let beverage = document.querySelector('#beverage').value;
+    let quantity = document.querySelector('#quantity').value;
+
+    
+    p.innerHTML = 'test';
+    cust.innerHTML = "test";
+
+};
